606bf86eec70ecf253299de489130d6d4e921eba
CHAITANYA SINGLA <chetnyasinglaa@gmail.com>
2026-02-03 19:03:26 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i;
+    int a[100];
+    int comparisons = 0;
+    int found = 0;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &k);
+
+    for (i = 0; i < n; i++) {
+        comparisons++;
+        if (a[i] == k) {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if (found == 0)
+        printf("Not Found\n");
+
+    printf("Comparisons = %d", comparisons);
+
+    return 0;
+}
