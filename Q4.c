d56813d96cf040e9a8145fc6fc8a461343348a62
CHAITANYA SINGLA <chetnyasinglaa@gmail.com>
2026-02-05 10:06:55 +0530

    Add program to reverse an array
    Implement array reversal in C

---
+++ Q4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a[100];
+    int left, right, temp;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    left = 0;
+    right = n - 1;
+
+    while (left < right) {
+        temp = a[left];
+        a[left] = a[right];
+        a[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
