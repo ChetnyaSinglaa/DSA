3836924e3b465808fd1a883f0330fe507c2bd5be
CHAITANYA SINGLA <chetnyasinglaa@gmail.com>
2026-02-09 21:06:19 +0530

    Implement string reversal in Q9.c

---
+++ Q9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    char s[100];
+    
+    printf("Enter a string: ");
+    fgets(s,sizeof(s),stdin);
+    int len=strlen(s);
+    
+    for(int i=0;i<len/2;i++){
+        char temp=s[i];
+        s[i]=s[len-i-1];
+        s[len-i-1]=temp;
+    }
+    
+    printf("%s",s);
+
+    return 0;
+}
