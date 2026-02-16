c3a681be4d78c289f05db26584667bc3e2abd0f3
CHAITANYA SINGLA <chetnyasinglaa@gmail.com>
2026-02-16 22:10:15 +0530

    Implement array element counting program
    This program counts the occurrences of each number in an array and prints them, ignoring duplicates.

---
+++ Q16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    
+    int arr[n];
+    printf("Enter array: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]==-1) continue;
+        int count=1;
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){
+            count++;
+            arr[j]=-1;
+            }
+        }
+        printf("%d:%d ",arr[i],count);
+    }
+
+    return 0;
+}
