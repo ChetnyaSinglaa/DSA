0ea745192741028180be3f8221885bae6a73ca49
CHAITANYA SINGLA <chetnyasinglaa@gmail.com>
2026-02-22 21:22:42 +0530

    Add program to count subarrays with zero sum

---
+++ Q20.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    
+    int arr[n];
+    printf("Enter the elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int count=0;
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum=sum+arr[j];
+            if(sum==0)
+            count++;
+        }
+    }
+    printf("%d",count);
+    
+    return 0;
+}
